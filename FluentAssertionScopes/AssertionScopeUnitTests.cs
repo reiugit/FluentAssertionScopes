0c3e6258e29991013a1e61e5e02edb238b948558
rei <67372053+reiugit@users.noreply.github.com>
2024-08-10 10:12:08 +0200

    Added result.

--- FluentAssertionScopes/AssertionScopeUnitTests.cs
+++ FluentAssertionScopes/AssertionScopeUnitTests.cs
@@ -8,15 +8,15 @@ public class AssertionScopeUnitTests
     [Fact]
     public void MultipleFails_ShouldBe_ListedAtOnce()
     {
-        (int number, string error) = (0, "Custom error");
+        (int number, string error) result = (0, "Custom error");
 
         // starting a scope for multiple assertions, so that
         // not only the first failure will be shown, but all of them.
 
         using (new AssertionScope())
         {
-            number.Should().NotBe(0);
-            error.Should().BeNullOrEmpty();
+            result.number.Should().NotBe(0);
+            result.error.Should().BeNullOrEmpty();
             // Both assertions will throw together, at the
             // moment this assertion-scope is disposed.
         }
